aeec64fc65890b4e02c2595a3aff3b68547b8399
codewithpatel <codewithashutoshpatel@gmail.com>
2021-11-30 01:59:20 +0530

    Razor pay

---
+++ android/app/src/main/kotlin/com/example/code_with_patel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.code_with_patel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+late   var _razorpay;
+  var amountController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    super.initState();
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    // Do something when payment succeeds
+    print("Payment Done");
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    // Do something when payment fails
+    print("Payment Fail");
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    // Do something when an external wallet is selected
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: const CupertinoNavigationBar(
+        middle: Text("CodeWithPatel"),
+      ),
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: TextField(
+                controller: amountController,
+                decoration:
+                    const InputDecoration(hintText: "Enter your Amount"),
+              ),
+            ),
+            CupertinoButton(
+                color: Colors.grey,
+                child: Text("Pay Amount"),
+                onPressed: () {
+                  ///Make payment
+                  var options = {
+                    'key': "rzp_test_xvlZZBGCo0SzL0",
+                    // amount will be multiple of 100
+                    'amount': (int.parse(amountController.text) * 100)
+                        .toString(), //So its pay 500
+                    'name': 'Code With Patel',
+                    'description': 'Demo',
+                    'timeout': 300, // in seconds
+                    'prefill': {
+                      'contact': '8787878787',
+                      'email': 'codewithpatel@gmail.com'
+                    }
+                  };
+                  _razorpay.open(options);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _razorpay.clear();
+    super.dispose();
+  }
+}
